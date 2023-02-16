85828510d7bfb12fbebda373c03d67633fe6ffa1
fmolliet <fmolliet@hotmail.com>
2023-02-15 23:22:43 -0300

    feat: Adicionado conteudo da aula 3 - if let, Option e generics

---
+++ src/aula3.rs
@@ -0,0 +1,44 @@
+fn conteudo_opcional(){
+    let conteudo_arquivo = ler_arquivo(String::from(""));
+    // É necessário tratar se irá retornar ou None ou Some
+    // Por estar no Heap ele precisa fazer borrow, usando o &
+    println!("{}", match &conteudo_arquivo {
+        None => "Arquivo não existe",
+        Some(valor) => valor
+    });
+    // Essa sintaxe faz a validação e exibe arrays, enum 
+    // Isso é a exibição de debug no Rust.
+    println!("{:?}", conteudo_arquivo );
+    
+    // Em enum precisamos passar os tipos, com options não precisamos passar o tipo
+    
+    // caso de errado ele nao executa 
+    // é um match com um unico valor
+    // Tambem tem como usar um While let
+    if let Some(valor) = conteudo_arquivo {
+        // Só vai executar quando entrar na condicional
+        println!("Agora, tem certeza de que ha o valor {}", valor);
+    }
+}
+
+// Generics server para criar 
+// template para o compilador utilizar para o tipos declarados
+enum GenericOption<T> {
+    Some( T ),
+    None
+}
+
+enum GenericResult <S, E> {
+    Ok(S),
+    Err(E)
+}
+
+fn ler_arquivo(caminho: String) -> Option<String> {
+    Some(String::from("conteudo do arquivo"))
+    // Ou podemos retornar None
+}
+
+
+pub fn main() {
+    conteudo_opcional();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod aula1;
 mod aula2;
+mod aula3;
 
 fn main() {
-    //aula1::main();
-    aula2::main();
+    // aula1::main();
+    // aula2::main();
+    aula3::main();
 }
